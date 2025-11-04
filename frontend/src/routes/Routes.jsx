129bdd3cb9005aa86c86007ef0ecab04810ddd53
dimuthuY <it22122964@my.sliit.lk>
2025-11-04 15:43:57 +0530

    Prescription adding part

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -2,6 +2,7 @@ import Dashboard from '../pages/Dashboard.jsx'
 import LogIn from '../pages/LogIn.jsx'
 import Register from '../pages/Register.jsx'
 import ProtectedTestPage from '../pages/ProtectedTestPage.jsx'
+import Prescription_Mainpage from '../pages/Prescription_Mainpage.jsx'
 import { createProtectedRoutes } from '../auth/auth.jsx'
 
 const unprotected_routes = [
@@ -12,7 +13,8 @@ const unprotected_routes = [
 
 const protected_routes = createProtectedRoutes([
   { path: '/dashboard', element: <Dashboard /> },
-  { path: '/e', element: <ProtectedTestPage /> }
+  { path: '/e', element: <ProtectedTestPage /> },
+  { path: '/prescription', element: <Prescription_Mainpage /> },
 ])
 
 export { unprotected_routes, protected_routes }
