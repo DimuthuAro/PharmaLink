4d0c64987bd004d2a55e35dec05c63fcddb2ea05
Apsara2002 <125apsararanahansi@gmail.com>
2025-12-03 19:45:10 +0530

    updated main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './main.css'
-import RouteList from './RouteList.jsx'
-import { AuthProvider } from './auth/auth.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./main.css";
+import RouteList from "./RouteList.jsx";
+import { AuthProvider } from "./auth/auth.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <AuthProvider>  {/* This makes auth available to entire app */}
+createRoot(document.getElementById("root")).render(
+  <AuthProvider>
     <StrictMode>
       <RouteList />
     </StrictMode>
   </AuthProvider>
-)
+);
